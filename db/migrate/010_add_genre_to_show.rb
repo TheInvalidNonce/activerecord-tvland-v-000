c77c8261c9d5123599e482198591cde312c158fa
Janusz Szubert <theinvalidnonce@gmail.com>
2017-10-16 21:33:37 +0000

    automatically backed up by learn

--- db/migrate/010_add_genre_to_show.rb
+++ db/migrate/010_add_genre_to_show.rb
@@ -1,5 +1,5 @@
-class AddSeasonToShows < ActiveRecord::Migration[4.2]
+class AddGenreToShows < ActiveRecord::Migration[4.2]
   def change
-    add_column :shows, :season, :string
+    add_column :shows, :genre, :string
   end
 end
