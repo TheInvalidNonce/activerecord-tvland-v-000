9a87edb4d8c25d073e8db207ec08989104e7c3ae
Janusz Szubert <theinvalidnonce@gmail.com>
2017-10-16 21:06:33 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -3,7 +3,7 @@ class CreateCharacters < ActiveRecord::Migration
     create_table :characters do |t|
       #primary key of :id is created for us!
       t.string :name
-      t.string :show_id
+      t.integer :show_id
     end
   end
 end
